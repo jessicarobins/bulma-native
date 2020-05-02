95bb44dbead1ce084d4a3c291d9bb5f10df67a61
Jess Solka <jessrrobins@gmail.com>
2020-05-02 14:21:32 -0400

    omg i can't believe this bs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming';
 import { DocsContainer } from '@storybook/addon-docs/blocks';
 import { jsxDecorator } from 'storybook-addon-jsx';
 import { withKnobs } from '@storybook/addon-knobs';
-import { addDecorator, addParameters, configure } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
 import FontAwesome5 from 'react-native-vector-icons/dist/FontAwesome';
 import { IconProvider, ThemeProvider, theme } from '../src/theme';
 
@@ -17,6 +17,7 @@ addDecorator((getStory) => (
 
 addParameters({
   docs: {
+    container: DocsContainer,
     extractComponentDescription: (_target, parameters) => {
       try {
         return parameters.component.__docgenInfo.description;
@@ -26,71 +27,23 @@ addParameters({
     },
   },
   options: {
+    storySort: (a, b) => {
+      const sectionA = a[1].id.split('-')[0];
+      const sectionB = b[1].id.split('-')[0];
+
+      return sectionB.localeCompare(sectionA);
+    },
+    theme: create({
+      base: 'light',
+      brandTitle: 'Bulma Native',
+      brandUrl: 'https://jessicarobins.github.io/bulma-native',
+      // To control appearance:
+      // brandImage: 'http://url.of/some.svg',
+    }),
     showPanel: true,
+    showRoots: true,
     panelPosition: 'bottom',
-    showAddonPanel: true,
+    // showAddonPanel: true,
     // addonPanelInRight: true,
   },
-});
-
-addons.setConfig({
-  /**
-   * show story component as full screen
-   * @type {Boolean}
-   */
-  isFullscreen: false,
-
-  /**
-   * display panel that shows a list of stories
-   * @type {Boolean}
-   */
-  showNav: true,
-
-  /**
-   * display panel that shows addon configurations
-   * @type {Boolean}
-   */
-  showPanel: true,
-
-  /**
-   * where to show the addon panel
-   * @type {('bottom'|'right')}
-   */
-  panelPosition: 'bottom',
-
-  /**
-   * sidebar tree animations
-   * @type {Boolean}
-   */
-  sidebarAnimations: true,
-
-  /**
-   * enable/disable shortcuts
-   * @type {Boolean}
-   */
-  enableShortcuts: true,
-
-  /**
-   * show/hide tool bar
-   * @type {Boolean}
-   */
-  isToolshown: true,
-
-  /**
-   * theme storybook, see link below
-   */
-  theme: undefined,
-
-  /**
-   * id to select an addon panel
-   * @type {String}
-   */
-  selectedPanel: undefined,
-
-  /**
-   * Select the default active tab on Mobile.
-   * 'sidebar' | 'canvas' | 'addons'
-   * @type {('sidebar'|'canvas'|'addons')}
-   */
-  initialActive: 'addons',
 });
